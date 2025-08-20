8df1a456f16b0c6bcf54729b682afe4d299963e8
LloydDafydd <31340777+LloydDafydd@users.noreply.github.com>
2025-08-20 15:44:00 +0100

    Update compileoF.sh

--- compileoF.sh
+++ compileoF.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-#SBATCH --job-name=compileoF
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
-#SBATCH --time=00-02:00:00
-#SBATCH --output=test_case_%j.out
-#SBATCH --error=test_case_%j.err
-
-cd /home/u35e/lloyddafydd.u35e/OpenFOAM-dev
-./Allwmake -q -j
-
+#!/bin/bash\#SBATCH --job-name=compileoF\#SBATCH --nodes=1\#SBATCH --ntasks-per-node=16\#SBATCH --cpus-per-task=1\#SBATCH --mem=32G\#SBATCH --time=00-02:00:00\#SBATCH --output=test_case_%j.out\#SBATCH --error=test_case_%j.err\./Allwmake -q -j 16
